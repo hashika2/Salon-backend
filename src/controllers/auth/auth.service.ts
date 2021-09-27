f2603641161338d8f39760131d7917bba25da6e4
hashika <m.g.hashikamaduranga@gmail.com>
2021-09-27 15:08:25 +0530

    add packges

--- src/controllers/auth/auth.service.ts
+++ src/controllers/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
+const bcrypt = require('bcryptjs');
+import { uuid } from 'uuidv4';
 import { Repository } from 'typeorm';
 import { UserEntity } from './auth.entity';
 import { User } from './user.interface';
@@ -13,20 +14,26 @@ export class AuthService {
 	async setRegister(user: User): Promise<UserEntity> {
 		const { username, email, password } = user;
 		const existData = await this.usersRepository.findOne({email});
-
-		if(existData.email){
+		if(existData || existData?.email){
 			throw new HttpException('User is Already exist', HttpStatus.BAD_REQUEST);
 		}
-
+		user.password = bcrypt.hashSync(password, 10);
+		user.userId = uuid();
 		return this.usersRepository.save(user);
 	}
 
-	setLogin(user: User): Object {
+	async setLogin(user: User): Promise<Object> {
 		const { email, password } = user;
 		const payload = { email: email, password: password };
+		const existData = await this.usersRepository.findOne({email});
 
+		if(!existData || !bcrypt.compareSync(password, existData?.password)){
+			throw new HttpException('Password is not matched', HttpStatus.BAD_REQUEST);
+		}
 		return {
-			access_token: this.jwtService.sign(payload)
+			access_token: this.jwtService.sign(payload),
+			username: existData.username,
+			userId: existData.userId
 		};
 	}
 }
