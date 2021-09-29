a635d41d35371d13500dedc96ece09ee9f4fb835
hashika <m.g.hashikamaduranga@gmail.com>
2021-09-29 13:41:12 +0530

    add store api

--- src/controllers/store/store.controller.ts
+++ src/controllers/store/store.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { query } from 'express';
 import { StoreService } from './store.service';
 
 @Controller('api/store')
 export class StoreController {
     constructor(private readonly storeService: StoreService){}
 
-    @Get('/')
-	storeData(): Object {
-		return this.storeService.getStoreData();
+    @Get('/all')
+	allStoreData(): Object {
+		return this.storeService.getAllStoreData();
+	}
+
+	@Get()
+	storeData(@Query() query): Object {
+		return this.storeService.getStoreData(query.id);
 	}
 }
