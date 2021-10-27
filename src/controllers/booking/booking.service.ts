79297b3a869722faae0501b1cf20936f068b9b62
hashika <m.g.hashikamaduranga@gmail.com>
2021-10-27 10:08:53 +0530

    push

--- src/controllers/booking/booking.service.ts
+++ src/controllers/booking/booking.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Equal, MoreThan, MoreThanOrEqual, Repository } from 'typeorm';
 import { BookingEntity } from './booking.entity';
 import { Booking } from './booking.interface';
 
 @Injectable()
 export class BookingService {
-	constructor(@InjectRepository(BookingEntity) @InjectRepository(BookingEntity) private bookingRepository: Repository<BookingEntity>) {}
+	constructor(
+		@InjectRepository(BookingEntity)
+		private bookingRepository: Repository<BookingEntity>
+	) {}
 
 	async getAllBookedData(): Promise<Object> {
-		// const res = await this.storeRepository.find();
-		// console.log
-		return [
+		// const res = await this.bookingRepository.find();
+		const res = [
 			{
 				day: new Date(),
 				time: [ '10.00', '1.00' ],
@@ -28,9 +30,36 @@ export class BookingService {
 				isDisable: false
 			}
 		];
+		return res.map((data) => ({
+			day: data.day,
+			time: data.time,
+			isDisable: data.time.length < 6 ? false : true
+		}));
 	}
 
-	async setBookingDate(booking: Booking):Promise<BookingEntity> {
-		return await this.bookingRepository.save(booking);
+	async setBookingDate(booking: Booking): Promise<Object> {
+		//get each month booked date times
+		const res = await this.bookingRepository.findOne({
+			where: {
+				date: Equal('2021-10-01 15:31:10')
+			}
+		});
+		console.log(res)
+		let pickArray = [];
+		// if (res && res.picktime.length>0) {
+		// 	pickArray = JSON.parse(res.picktime);
+		// 	console.log(pickArray)
+		// 	res.picktime = JSON.stringify(pickArray.push(booking.time));
+		// 	return await this.bookingRepository.save(res);
+		// }
+		console.log("*******")
+
+		const time = JSON.stringify(pickArray.push(booking.time));
+		const newBooking: Booking = {
+			userId: booking.userId,
+			date: booking.date,
+			time: time,
+		};
+		return await this.bookingRepository.save(newBooking);
 	}
 }
